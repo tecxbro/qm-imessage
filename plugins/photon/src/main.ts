c84471cd439d15f62eb92bb4df30e51a57411c9a
tecxbro <tecxbro@users.noreply.github.com>
2026-09-14 11:10:11 -0700

    Compose Photon provider runtime

---
+++ plugins/photon/src/main.ts
@@ -0,0 +1,8 @@
+export {
+  createPhotonAdapterComposition,
+  type PhotonAdapterComposition,
+  type PhotonAdapterCompositionDeps,
+  type PhotonAdapterConnection,
+  type PhotonAdapterConnectionResult,
+} from "./composition.ts";
+export { createPhotonCoreHttpFactory, type PhotonCoreHttpFactory } from "./http.ts";
